1be9465096c2064c764f79b32e07632c275f4968
DamianFalaFalkowski <dfalkows@hotmail.com>
2025-02-06 19:22:00 +0100

    add dc logger

--- dist/commands/generate-transfer-msg.js
+++ dist/commands/generate-transfer-msg.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.execute = void 0;
 const discord_js_1 = require("discord.js");
+const dc_logger_1 = __importDefault(require("../utils/dc-logger"));
+const allowedRoles = ['member', 'admin', 'moderator', 'owner', 'honored-member', 'super-moderator'];
 module.exports = {
     data: new discord_js_1.SlashCommandBuilder()
         .setName('generate-transfer-msg')
@@ -14,12 +19,28 @@ module.exports = {
     }
 };
 const execute = async (interaction) => {
-    // Sprawdzenie czy przekazana rola istnieje w systemie i czy jest rolą ozanczoną jako do kupienia
-    if (interaction.guild.roles.cache.find(role => role.name === interaction.options.data[0].role.name) === undefined)
-        return interaction.reply('Rola nie istnieje w systemie.');
-    if (interaction.options.data[0].role.name.charAt(0) !== '+')
-        return interaction.reply('Rola nie jest przeznaczona do kupienia.');
+    dc_logger_1.default.logCommand(interaction);
     let reply = { content: undefined, flags: discord_js_1.MessageFlags.Ephemeral };
+    if (!interaction.isCommand())
+        return;
+    // Sprawdzenie czy uzytkownik na uprawnienia do uruchomienia polecenia
+    const member = interaction.member; // TODO: resp mess
+    if (!member || !('roles' in member))
+        return; // TODO: resp mess
+    const roles = member.roles.cache;
+    if (!roles.some(role => allowedRoles.includes(role.name)))
+        return; // TODO: resp mess
+    // Sprawdzenie czy przekazana rola istnieje w systemie ...
+    if (interaction.guild.roles.cache.find(role => role.name === interaction.options.data[0].role.name) === undefined) {
+        reply.content = 'Rola nie istnieje w systemie.';
+        return interaction.reply(dc_logger_1.default.logReplyAndReturn(interaction, reply));
+    }
+    // ... i czy jest rolą ozanczoną jako do kupienia
+    if (interaction.options.data[0].role.name.charAt(0) !== '+') {
+        reply.content = 'Rola nie jest przeznaczona do kupienia.';
+        return interaction.reply(dc_logger_1.default.logReplyAndReturn(interaction, reply));
+    }
+    // rozpoczęcie generowania wiadomości
     let itemName = interaction.options.data.find((x) => x !== undefined).role.name;
     let globalName = interaction.user.globalName;
     let userId = interaction.user.id;
@@ -28,15 +49,13 @@ const execute = async (interaction) => {
     const ValidRespMess = /[a-z A-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ()_=\-:'.?\/\\]+$/.test(respMess);
     if (!ValidRespMess) {
         reply.content = 'Nazwa roli zawiera niedozwolone znaki.';
-        return interaction.reply(reply);
+        return interaction.reply(dc_logger_1.default.logReplyAndReturn(interaction, reply));
     }
-    console.log(respMess.length);
-    console.log(respMess.toString());
     // Sprawdzenie czy respMess nie jest dłuższa niż 140 znaków
     if (respMess.length > 140) {
         reply.content = 'Wiadomość jest zbyt długa. Maksymalna długość to 140 znaków.';
         reply.flags = discord_js_1.MessageFlags.Ephemeral;
-        return interaction.reply(reply);
+        return interaction.reply(dc_logger_1.default.logReplyAndReturn(interaction, reply));
     }
     reply.content = `Aby dokonać zakupu produktu **${itemName.toString()}** wklej tą wiadomość w tytule przelewu:\n\n **${respMess}**`;
     // Utwórz przycisk do kopiowania
@@ -46,10 +65,10 @@ const execute = async (interaction) => {
         .setLabel('Copy')
         .setStyle(discord_js_1.ButtonStyle.Primary));
     reply.components = [row];
-    await interaction.reply(reply);
+    await interaction.reply(dc_logger_1.default.logReplyAndReturn(interaction, reply));
 };
 exports.execute = execute;
-// TODO: sprawić, eby wiadomość była widoczna tylko dla pytającego
+// TODO: dodać logowanie
 // TODO: dodać przycisk do kopiowania
 // TODO: mona te dodać rodzaj produktu
 // TODO: ograniczyć tylko do ról po rozpoczętej walidacji (@member)
---
+++ src/utils/dc-logger.ts
@@ -0,0 +1,46 @@
+import { CommandInteraction, InteractionReplyOptions } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+
+export class DcLogger {
+    private logFilePath: string;
+
+    constructor() {
+        this.logFilePath = path.join(__dirname, '../../logs/discord.log');
+        this.ensureLogFileExists();
+    }
+
+    private ensureLogFileExists() {
+        if (!fs.existsSync(this.logFilePath)) {
+            fs.writeFileSync(this.logFilePath, '', { flag: 'wx' });
+        }
+    }
+
+    private logToFile(message: string) {
+        const timestamp = new Date().toISOString();
+        fs.appendFileSync(this.logFilePath, `[${timestamp}] ${message}\n`);
+    }
+
+    public logCommand(interaction: CommandInteraction) {
+        const user = interaction.user.tag;
+        const commandName = interaction.commandName;
+        const options = interaction.options.data.map(option => `${option.name}: ${option.value}`).join(', ');
+        const logMessage = `Command executed by ${user}: /${commandName} ${options}`;
+        this.logToFile(logMessage);
+    }
+
+    public logReplyAndReturn(interaction: CommandInteraction, reply: InteractionReplyOptions): InteractionReplyOptions {
+        const user = interaction.user.tag;
+        const replyContent = reply.content || 'No content';
+        const logMessage = `Reply to ${user}: ${replyContent}`;
+        this.logToFile(logMessage);
+        return reply;
+    }
+
+    public logError(error: Error) {
+        const logMessage = `Error: ${error.message}\nStack: ${error.stack}`;
+        this.logToFile(logMessage);
+    }
+}
+
+export default new DcLogger();
