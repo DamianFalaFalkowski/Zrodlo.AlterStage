4a8250cbce40aad29521fbdd90694dfa2b45ea42
DamianFalaFalkowski <dfalkows@hotmail.com>
2025-02-07 02:08:59 +0100

    generate transfer msg improvements

--- src/commands/generate-transfer-msg.ts
+++ src/commands/generate-transfer-msg.ts
@@ -23,47 +23,50 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
     if (!interaction.isCommand()) return;
 
     // Sprawdzenie czy uzytkownik na uprawnienia do uruchomienia polecenia
-    const member = interaction.member; // TODO: resp mess
-    if (!member || !('roles' in member)) return; // TODO: resp mess
+    const member = interaction.member;
+    if (!member || !('roles' in member)) {
+        reply.content = 'Brak uprawnień do uruchomienia tego polecenia.';
+        return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
+    }
     const roles = (member.roles as GuildMemberRoleManager).cache;
-    if (!roles.some(role => allowedRoles.includes(role.name))) return; // TODO: resp mess
+    if (!roles.some(role => allowedRoles.includes(role.name))) {
+        reply.content = 'Brak uprawnień do uruchomienia tego polecenia.';
+        return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
+    }
 
     // Sprawdzenie czy przekazana rola istnieje w systemie ...
-    if (interaction.guild!.roles.cache.find(role => role.name === interaction.options.data[0].role!.name) === undefined) 
-    {
-        reply.content = 'Rola nie istnieje w systemie.';
+    if (interaction.guild!.roles.cache.find(role => role.name === interaction.options.data[0].role!.name) === undefined) {
+        reply.content = 'Rola którą próbujesz zakupić nie istnieje w systemie.';
         return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
     }
 
     // ... i czy jest rolą ozanczoną jako do kupienia
-    if(interaction.options.data[0].role!.name.charAt(0) !== '+') 
-    {
-        reply.content = 'Rola nie jest przeznaczona do kupienia.';
-        return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply)); 
+    if (interaction.options.data[0].role!.name.charAt(0) !== '+') {
+        reply.content = 'Rola którą próbujesz zakupić nie jest przeznaczona do kupienia.';
+        return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
     }
-        
+
 
     // rozpoczęcie generowania wiadomości
     let itemName = interaction.options.data.find((x: any) => x !== undefined)!.role!.name;
     let globalName = interaction.user.globalName!;
     let userId = interaction.user.id;
-    let respMess = userId.toString() + ' ' + globalName.toString() + ' ' + itemName.toString();
+    let generatedTransferMessage = userId.toString() + ' ' + globalName.toString() + ' ' + itemName.toString();
 
-    // Sprawdzenie czy respMess zawiera tylko dozwolone znaki
-    const ValidRespMess = /[a-z A-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ()_=\-:'.?\/\\]+$/.test(respMess);
+    // Sprawdzenie czy generatedTransferMessage zawiera tylko dozwolone znaki
+    const ValidRespMess = /[a-z A-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ()_=\-:'.?\/\\]+$/.test(generatedTransferMessage);
     if (!ValidRespMess) {
-        reply.content = 'Nazwa roli zawiera niedozwolone znaki.';
-        return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
+        // Jeśli tak to zastąp niedozwolone znaki podkreślnikiem
+        generatedTransferMessage = generatedTransferMessage.replace(/[^a-z A-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ()_=\-:'.?\/\\]+/g, '_');
     }
 
-    // Sprawdzenie czy respMess nie jest dłuższa niż 140 znaków
-    if (respMess.length > 140) {
-        reply.content = 'Wiadomość jest zbyt długa. Maksymalna długość to 140 znaków.';
-        reply.flags = MessageFlags.Ephemeral;
-        return interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
+    // Sprawdzenie czy generatedTransferMessage nie jest dłuższa niż 140 znaków
+    if (generatedTransferMessage.length > 140) {
+        // JHeśli tak to skróć generatedTransferMessage do 140 znaków
+        generatedTransferMessage = generatedTransferMessage.substring(0, 140);
     }
 
-    reply.content = `Aby dokonać zakupu produktu **${itemName.toString()}** wklej tą wiadomość w tytule przelewu:\n\n **${respMess}**`
+    reply.content = `Aby dokonać zakupu produktu **${itemName.toString()}** wklej tą wiadomość w tytule przelewu:\n\n **${generatedTransferMessage}**`
 
     // Utwórz przycisk do kopiowania
     const row = new ActionRowBuilder<ButtonBuilder>()
@@ -77,7 +80,5 @@ export const execute = async (interaction: ChatInputCommandInteraction) => {
     await interaction.reply(dcLogger.logReplyAndReturn(interaction, reply));
 };
 
-// TODO: dodać logowanie
 // TODO: dodać przycisk do kopiowania
 // TODO: mona te dodać rodzaj produktu
-// TODO: ograniczyć tylko do ról po rozpoczętej walidacji (@member)
